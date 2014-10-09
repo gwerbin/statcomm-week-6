f7cd64bedab3e0ae19d5faec23b8193ebe83ec4f
rebeccahch <hueichunghuang@gmail.com>
2014-10-09 00:27:52 -0400

    not quite sure why the regression plots are the same with/without capital

--- server.R
+++ server.R
@@ -12,16 +12,17 @@ shinyServer(function (input, output, session) {
                       choices = regression_variables[regression_variables != input$x]
     )})
     
-  snu_coef <- reactive(snu_fit(input$x, input$y, dat = snu))
+  #snu_coef <- reactive(snu_fit(input$x, input$y, dat = snu))
   
   output$coefplot <- renderPlot({
-#     snu_coefplot(output$snu_coef <- snu_coef())
+#      snu_coefplot(output$snu_coef <- snu_coef())
 #      text(0.5, 0.5, paste(input$x, " ", input$y))
 #      text(0.2, 0.2, snu_fit(input$x, input$y))
-#    snu_coefplot(snu_fit(input$x, input$y))
-    if(input$withCap == TRUE) snu_coefplot(snu_fit(input$x, input$y, dat = snu[snu$capital!=1,]))
-    else snu_coefplot(snu_fit(input$x, input$y, dat = snu))
-    #text(0.5, 0.5, paste(input$withCap, "", input$x, "", input$sortby))
+#      snu_coefplot(snu_fit(input$x, input$y))
+    if(input$withCap == TRUE) {snu_coefplot(snu_fit(input$x, input$y, dat = snu))}
+    else {snu_coefplot(snu_fit(input$x, input$y, dat = snu[snu$capital!=1,]))}
+
+ #   text(0.5, 0.5, paste(input$withCap, "", input$x, "", input$sortby))
   })
   
 })
